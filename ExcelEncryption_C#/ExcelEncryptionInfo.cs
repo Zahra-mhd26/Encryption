711143b18828e0b92c63b1b6352482a3c74039b6
Zahra <mahmoudi.zahra.1996@gmail.com>
2025-07-10 10:24:13 +0330

    Start

---
+++ ExcelEncryption_C#/ExcelEncryptionInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace ExcelEncryption
+{
+    public class ExcelEncryptionInfo : GH_AssemblyInfo
+    {
+        public override string Name => "ExcelEncryption";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("24f91c2c-41d2-4634-882a-9d000bcb9180");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
